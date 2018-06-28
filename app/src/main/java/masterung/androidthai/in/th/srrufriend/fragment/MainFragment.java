8dafb05e070fbe0ecbc132ff472d7340f58b80ba
wijittrapo <wijittrapo@srru.ac.th>
2018-06-28 13:27:27 +0700

    Initial commit

---
+++ app/src/main/java/masterung/androidthai/in/th/srrufriend/fragment/MainFragment.java
@@ -0,0 +1,43 @@
+package masterung.androidthai.in.th.srrufriend.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import masterung.androidthai.in.th.srrufriend.R;
+
+public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    //Register Controller
+
+        TextView textView = getView().findViewById(R.id.txtNameRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            // Replace Fragment
+
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment,new RegisterFragment()).commit();
+            }
+        });
+
+    }// Main Method
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}   // Main Class
